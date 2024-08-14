868e60b7da83c14300f49f742750f0462f106a94
blackdevil <madushanchathuranghe192@gmail.com>
2024-08-14 22:43:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomeSection from './components/Home/HomeSection/HomeSection'
-import AboutSection from './components/Home/AboutSection/AboutSection'
-import TimelineSection from './components/Home/TimelineSection/TimelineSection'
+import Navbar from './components/NavBar/NavBar'
 
 function App() {
    return (
     <>
       <div>
+        <Navbar/>
         <h1>Repairs in Progress</h1>
       </div>
     </>
