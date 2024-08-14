6d4ced2c31ae7a85140c8381f7ca44665556199b
blackdevil <madushanchathuranghe192@gmail.com>
2024-08-14 22:36:13 +0530

    Folder Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomeSection from './components/Home/HomeSection'
-import AboutSection from './components/Home/AboutSection'
+import HomeSection from './components/Home/HomeSection/HomeSection'
+import AboutSection from './components/Home/AboutSection/AboutSection'
 import TimelineSection from './components/Home/TimelineSection'
 
 function App() {
