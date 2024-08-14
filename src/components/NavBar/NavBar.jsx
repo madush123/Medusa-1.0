dde0d0eda4c38fb5d429d8d50bb75160869747e8
blackdevil <madushanchathuranghe192@gmail.com>
2024-08-14 22:41:12 +0530

    Navbar Update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBar = () => {
+function Navbar() {
   return (
-    <div>
-        <h1>This is Nav bar</h1>      
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
